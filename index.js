1548d056df7d43ad8b557379fa09e81a836b1fcc
kamendergangwar <110359695+kamendergangwar@users.noreply.github.com>
2022-08-30 00:24:28 +0530

    added the database and ejs

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const http = require('http');
+const express = require('express');
+const port = 8000;
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+// Redirect all to index.js inside routes directory
+app.use('/', require('./routes'));
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+// Setting view engine as ejs
+app.set('view engine', 'ejs');
+// Setting path for views
+app.set('views', './views');
+
+
+// to use static files, present in assets directory
+app.use(express.static('assets'));
+
+// Setting express to listen to port 8000
+app.listen(port, function(err){
+    if(err){
+        console.log("Error Occurred while trying to run server on port : ", port);
+        return;
+    }
+    console.log("Express Server is up and Running on port : ", port);
 });
